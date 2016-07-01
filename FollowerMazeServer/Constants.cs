b0acaa110fc1471b28937910557a90ea34a020c0
Thanh Phu <thanhphu@outlook.com>
2016-07-01 17:17:02 +0900

    Add documentation

--- FollowerMazeServer/Constants.cs
+++ FollowerMazeServer/Constants.cs
@@ -5,10 +5,10 @@ namespace FollowerMazeServer
     public static class Constants
     {
         public static readonly int BufferSize = 1024; // in bytes
-        public static readonly int WorkerDelay = 10; // in ms
+        // Time between each iteration inside client's thread. Messages will only be sent out once during this perioud
+        public static readonly int WorkerDelay = 100; // in ms. 
         public static readonly IPAddress IP = IPAddress.Any;
         public static readonly int EventSourcePort = 9090;
         public static readonly int ClientConnectionPort = 9099;
-        public static readonly int RetryLimit = 5; // Number of times a payload will be retried before it gets discarded
     }
 }
